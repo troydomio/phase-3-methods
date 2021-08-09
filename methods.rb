15cdfd7477ee9f5cd738dc9114188b2fa9e54b4d
Troy <troyakins99@yahoo.com>
2021-08-08 22:05:33 -0500

    commit tree 961ea0e43845b41a353379b729e7ef825db838a6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer 
+puts "Hello, programmer!"
+end 
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end 
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+        return nil;
+    else 
+        number / 2
+    end
+end
+
+# def method_name(args)
+#     if condition == true
+#         run_some_code
+#     else
+#         run_some_other_code
+#     end
+# end
